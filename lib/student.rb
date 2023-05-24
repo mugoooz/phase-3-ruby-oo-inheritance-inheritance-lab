65e0bc8f7fafb48a7efb66115333b3cb7ab2549d
Eric Mugo <mugoericpg20@gmail.com>
2023-05-24 14:57:03 +0300

    Made minor changes

--- lib/student.rb
+++ lib/student.rb
@@ -1,17 +1,14 @@
 require_relative 'user'
 
 class Student < User
-    def initialize(first_name, last_name)
-      super(first_name, last_name)
-      @knowledge = []
-    end
-  
-    def learn(string)
-      @knowledge << string
-    end
-  
-    def knowledge
-      @knowledge
-    end
+  attr_reader :knowledge
+
+  def initialize(first_name, last_name)
+    super(first_name, last_name)
+    @knowledge = []
   end
-  
+
+  def learn(string)
+    @knowledge << string
+  end
+end
