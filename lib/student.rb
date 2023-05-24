dc8d15629d21130a405f55bc4402aaf91e1bf0d5
Eric Mugo <mugoericpg20@gmail.com>
2023-05-24 14:38:36 +0300

    Inherited from user

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,15 @@
-class Student 
+class Student < User
+    attr_reader :knowledge
 
+    def initialize(first_name, last_name)
+        super(first_name,last_name)
+        @knowledge = []
+    end
+
+    def learn(string)
+        @knowledge << string
+    end
 end
+
+
+
